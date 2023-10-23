8c1b7950878cbe87296670b1bc92974dfc8fc036
Zhoucai <nizhoucai@gmail.com>
2023-10-23 15:19:25 -0400

    added editing feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Home from './pages/Home';
 import Game from './pages/Game';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,144 @@
+import { useState } from 'react'
 import React from 'react'
 import Header from '../components/Header'
+import Slider from '../components/Slider'
+import SidebarItem from '../components/SliderbarItem'
+import edited from '../assets/Chai-Edited.jpg'
+import pre from '../assets/Chai000724-R2-077-37.jpg'
+import "../App.scss";
+
+const DEFAULT_OPTIONS = [
+  {
+    name: 'Brightness',
+    property: 'brightness',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Contrast',
+    property: 'contrast',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Saturation',
+    property: 'saturate',
+    value: 100,
+    range: {
+      min: 0,
+      max: 200
+    },
+    unit: '%'
+  },
+  {
+    name: 'Grayscale',
+    property: 'grayscale',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Sepia',
+    property: 'sepia',
+    value: 0,
+    range: {
+      min: 0,
+      max: 100
+    },
+    unit: '%'
+  },
+  {
+    name: 'Hue Rotate',
+    property: 'hue-rotate',
+    value: 0,
+    range: {
+      min: 0,
+      max: 360
+    },
+    unit: 'deg'
+  },
+  {
+    name: 'Blur',
+    property: 'blur',
+    value: 0,
+    range: {
+      min: 0,
+      max: 20
+    },
+    unit: 'px'
+  }
+]
 
 const Game = () => {
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
+  const [options, setOptions] = useState(DEFAULT_OPTIONS)
+  const selectedOption = options[selectedOptionIndex]
+
+  function handleSliderChange({ target }) {
+    setOptions(prevOptions => {
+      return prevOptions.map((option, index) => {
+        if (index !== selectedOptionIndex) return option
+        return { ...option, value: target.value }
+      })
+    })
+  }
+
+  function getImageStyle() {
+    const filters = options.map(option => {
+      return `${option.property}(${option.value}${option.unit})`
+    })
+
+    return { filter: filters.join(' ') }
+  }
+
   return (
     <div>
+
       <Header>  </Header>
+
+      <div className="container">
+        {/* <div className="main-image" style={getImageStyle()} /> */}
+        <div className='photoContainer'>
+          <div className='photo'>
+            <p> Pre </p>
+            <img src={pre} alt="pre edit pics" style={getImageStyle()} />
+          </div>
+          <div className='photo'>
+            <p> Goal </p>
+            <img src={edited} alt="edited pics" />
+          </div>
+        </div>
+
+        <div className="sidebar">
+          {options.map((option, index) => {
+            return (
+              <SidebarItem
+                key={index}
+                name={option.name}
+                active={index === selectedOptionIndex}
+                handleClick={() => setSelectedOptionIndex(index)}
+              />
+            )
+          })}
+        </div>
+        <Slider
+          min={selectedOption.range.min}
+          max={selectedOption.range.max}
+          value={selectedOption.value}
+          handleChange={handleSliderChange}
+        />
+      </div>
     </div>
   )
 }
