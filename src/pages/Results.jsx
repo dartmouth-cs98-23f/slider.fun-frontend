a78e71809436e1d434a4b9df6cbad98e79928bd7
JohanCruzHernandez <johan.j.cruz.hernandez.24@dartmoouth.edu>
2023-11-06 17:05:00 -0500

    adding back button to results page and adding route to return to the game page once it is clicked

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Header from '../components/Header';
+import { useNavigate } from "react-router-dom";
 import stockPhoto from "../images/stockphoto.png"
-import preImage from "../images/pre.jpeg";
-import goalImage from "../images/goal.jpeg";
+import preImage from "../assets/pre.jpeg";
+import goalImage from "../assets/goal.jpeg";
 import "../styles/results.scss";
 
 const Results = () => {
+    const navigate = useNavigate();
     return (
         <>
             <Header> </Header>
             <div className="resultsModal">
                 <div className="headerModal">
                     <h2>So close!</h2>
-                    <h3>Score</h3>
+                    <h3>Score []</h3>
                 </div>
                 <div className="imagesModal-container">
                     <div>
@@ -25,6 +27,9 @@ const Results = () => {
                 <div className="buttonsModal">
                     <button className="shareResultsButton">share results</button>
                     <button className="viewStatsButton">view stats</button>
+                </div>
+                <div className="backButton">
+                    <button onClick={() => {navigate("/game")}}>back</button>
                 </div>
             </div>
         </>
