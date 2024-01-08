b697c85b80a8d63d53660867bd2121386a6fde60
JohanCruzHernandez <johan.j.cruz.hernandez.24@dartmoouth.edu>
2024-01-08 17:24:28 -0500

    Uploaded images now display on page and are also uploaded to Firebase. Next step is to figure out how to get the URL of the images that were uploaded and to improve the UI of this page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { storage } from '../firebase';
 // ref references the location to upload image
 // uploadBytes funtion uploads photo
-import { ref, uploadBytes } from 'firebase/storage';
+// listAll retruns all the files in a given path
+// gets URL from downloaded items
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
 // v4 generates random string of characters
 import { v4 } from 'uuid';
 
@@ -11,6 +13,10 @@ const Profile = () => {
 
     // connected to the 'choose file' input button
     const [imageUpload, setImageUpload] = useState(null)
+    const [imageList, setImageList] = useState([]);
+    
+    // make a reference to all the images stored in the given path
+    const imageListRef = ref(storage, "selfUploadedImages")
 
     // This functio is called when the 'Upload Image' button is clicked
     // This function pushes the image to firebase
@@ -24,10 +30,23 @@ const Profile = () => {
         // note: image of the uploaded photo is given a random name
         const imageRef = ref(storage, `selfUploadedImages/$(imageUpload.name + v4()}`);
         // upload image to firebase
-        uploadBytes(imageRef, imageUpload).then(() => {
+        uploadBytes(imageRef, imageUpload).then((snapshot) => {
             alert("Image successfully uploaded");
+            getDownloadURL(snapshot.ref).then((url) => {
+                setImageList((prev) => [...prev, url]);
+            })
         })
     };
+
+    useEffect(() => {
+        listAll(imageListRef).then((response) => {
+            response.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImageList((prev) => [...prev, url]);
+                });
+            })
+        })
+    }, [])
 
     return (
         <div className="App">
@@ -37,6 +56,9 @@ const Profile = () => {
                 setImageUpload(event.target.files[0]);
             }}/>
             <button onClick={uploadImage}>Upload Image</button>
+            {imageList.map((url) => {
+                return <img src={url} />
+            })};
         </div>
     );
 };
