9314af33dd5740647ed0857731860d8bc52a4974
Zhoucai <nizhoucai@gmail.com>
2024-01-10 17:04:26 -0500

    added video background

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,18 @@ import domainLogo from "../assets/domain_logo.svg"
 const Login = () => {
   return (
     <div className='loginContainer'>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        id="video"
+        preload="auto"
+        style={{ width: '100%', height: '100%', objectFit: 'cover', opacity: '.8'}}
+      >
+        <source src="https://firebasestorage.googleapis.com/v0/b/sliderdotfun-3af7a.appspot.com/o/videos%2Fseq.mov?alt=media&token=fe8d69fb-7bf0-405f-bacb-ae38a5059d37" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className='formContainer'>
         <form>
           <img src={domainLogo} alt=""></img>
