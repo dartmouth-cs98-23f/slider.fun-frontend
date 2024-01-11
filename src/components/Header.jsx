9614c6f374a8a0babb8218e77c6f8dfd9c1e7d95
Kashan Mahmood <kashan.mahmood.24@dartmouth.edu>
2024-01-10 20:50:13 -0500

    made the nav bar more sleek. better UI for both tutorial and daily puzzle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,29 +5,30 @@ import { useNavigate } from 'react-router-dom';
 import TodaysDate from './TodaysDate';
 import { useLocation } from 'react-router-dom';
 
-
 const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const handleNavigate = (path) => {
     navigate(path);
   };
-  return (
 
+  return (
     <div className='headerContainer'>
       <div className='contentContainer'>
-        <div className="logoDateContainer">
-          <img onClick={() => handleNavigate("/")} src={domainLogo} alt="" width="150"></img>
-          {location.pathname.startsWith('/daily') && <TodaysDate />}
-
+        <div className="logoContainer">
+          <img onClick={() => handleNavigate("/")} src={domainLogo} alt="Domain Logo" width="150"></img>
         </div>
-        <nav className='headerNav'>
-          <button className="tutorialButton" onClick={() => handleNavigate("/tutorial/stage1")}>Tutorial Mode</button>
-          <button className="dailyButton" onClick={() => handleNavigate("/daily")}> Daily Puzzle </button>
+        <div className="dateContainer">
+          {location.pathname.startsWith('/daily') && <TodaysDate />}
+        </div>
+        <nav className='navContainer'>
+          <button className="navButton" onClick={() => handleNavigate("/tutorial/stage1")}>Tutorial Mode</button>
+          <button className="navButton" onClick={() => handleNavigate("/daily")}>Daily Puzzle</button>
         </nav>
       </div>
-    </div >
+    </div>
   )
 }
 
 export default Header
+
