3c4decbcd005636cb790d104d7f153742fc53bbe
JohanCruzHernandez <johan.j.cruz.hernandez.24@dartmoouth.edu>
2024-01-31 16:35:49 -0500

    adding self upload feature

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { storage } from '../firebase';
+// import "../styles/profile.scss";
+
+////////////////////////////////////////////////////////////////////////////////
+// Start of Firebase stuff
+////////////////////////////////////////////////////////////////////////////////
 // ref references the location to upload image
 // uploadBytes funtion uploads photo
 // listAll retruns all the files in a given path
@@ -10,55 +15,49 @@ import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
 import { v4 } from 'uuid';
 
 const Profile = () => {
+    const [imageUpload, setImageUpload] = useState(null);
+    const [imageUrls, setImageUrls] = useState([]);
+    const imagesListRef = ref(storage, "selfUploadedImages/");
 
-    // connected to the 'choose file' input button
-    const [imageUpload, setImageUpload] = useState(null)
-    const [imageList, setImageList] = useState([]);
-    
-    // make a reference to all the images stored in the given path
-    const imageListRef = ref(storage, "selfUploadedImages")
-
-    // This functio is called when the 'Upload Image' button is clicked
-    // This function pushes the image to firebase
-    const uploadImage = () => {
-        // no image was selected
+    const uploadFile = () => {
         if (imageUpload == null) return;
-
-        // upload image from webpage to firebase
-        // param1: storage refers to the storage of the slider.fun application
-        // param2: folder in firebase (images) followed by the name of the photo
-        // note: image of the uploaded photo is given a random name
-        const imageRef = ref(storage, `selfUploadedImages/$(imageUpload.name + v4()}`);
-        // upload image to firebase
+        const imageRef = ref(storage, `selfUploadedImages/${imageUpload.name + v4()}`);
         uploadBytes(imageRef, imageUpload).then((snapshot) => {
-            alert("Image successfully uploaded");
             getDownloadURL(snapshot.ref).then((url) => {
-                setImageList((prev) => [...prev, url]);
-            })
-        })
+                setImageUrls((prev) => [...prev, url]);
+            });
+        });
     };
 
     useEffect(() => {
-        listAll(imageListRef).then((response) => {
+        listAll(imagesListRef).then((response) => {
             response.items.forEach((item) => {
                 getDownloadURL(item).then((url) => {
-                    setImageList((prev) => [...prev, url]);
+                    setImageUrls((prev) => [...prev, url]);
                 });
-            })
-        })
-    }, [])
+            });
+        });
+    }, []);
 
+    ////////////////////////////////////////////////////////////////////////////////
+    // End of Firebase stuff
+    ////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Start of addung UI components to the profile page
+    ////////////////////////////////////////////////////////////////////////////////
     return (
         <div className="App">
-            <input 
-            type="file" 
+        <input
+            type="file"
             onChange={(event) => {
                 setImageUpload(event.target.files[0]);
-            }}/>
-            <button onClick={uploadImage}>Upload Image</button>
-            {imageList.map((url) => {
-                return <img src={url} />
-            })};
+            }}
+            />
+        <button onClick={uploadFile}> Upload Image</button>
+        {imageUrls.map((url) => {
+            return <img src={url} />;
+        })}
         </div>
     );
 };
