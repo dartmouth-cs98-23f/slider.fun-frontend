e9b21861e0737431d299f052585991ef3da9db9a
JohanCruzHernandez <johan.j.cruz.hernandez.24@dartmoouth.edu>
2024-02-21 16:46:03 -0500

    adding array of scores to the scorecards (these are still hardcoded values but now the scorecards recieve an array of scores. Next step for this feature is to get the scores from the user

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import Daily from './pages/Daily';
 //import HeaderWrapper from './components/HeaderWrapper';
 import Tutorial from './pages/Tutorial';
 import ResultsPage from './components/ResultsPage';
+import Home from "./pages/Home"
+
 function App() {
   return (
     <BrowserRouter>
       {/* <HeaderWrapper /> */}
       <Routes classname="routesContainer">
-        <Route path="/">
-          {/* <Route index element={<Home />} /> */}
-          <Route path="game" element={<Game />} />
-          <Route path="/tutorial" element={<Tutorial />} />
-          <Route path="/daily" element={<Daily />} />
-          <Route path="/resultspage" element={<ResultsPage />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="game" element={<Game />} />
+        <Route path="/tutorial" element={<Tutorial />} />
+        <Route path="/daily" element={<Daily />} />
+        <Route path="/resultspage" element={<ResultsPage scores={[87,94,97,95,91,93, 96]} />} />
+
       </Routes>
     </BrowserRouter>
   );
--- src/components/ResultsPage.jsx
+++ src/components/ResultsPage.jsx
@@ -14,18 +14,28 @@ const ScoreCard = ({stage, score}) => {
     )
 }
 
+const ScoreSummary = ({ scores, onPlayAgain }) => {
+    return (
+        <div className="score-summary">
+            <div className="average-score">Average Score: []</div>
+            <button onClick={onPlayAgain}>Play Again</button>
+            <button>Share</button>
+        </div>
+    )
+}
+
 const ResultsPage = ({scores}) => {
     // Scores should be an object with keys as stage names and valuesas the score
     // For example: { Brightness: 96, Contrast: 98, Saturation: 99, Grayscale: 94, Sepia: }
     return (
         <div className="results-page">
-            <ScoreCard key="brightness" stage="brightness" score="95"></ScoreCard>
-            <ScoreCard key="contrast" stage="contrast" score="95"></ScoreCard>
-            <ScoreCard key="saturation" stage="saturation" score="95"></ScoreCard>
-            <ScoreCard key="grayscale" stage="grayscale" score="95"></ScoreCard>
-            <ScoreCard key="sepia" stage="sepia" score="95"></ScoreCard>
-            <ScoreCard key="hue rotate" stage="hue rotate" score="95"></ScoreCard>
-            <ScoreCard key="blur" stage="blur" score="95"></ScoreCard>
+            <ScoreCard key="brightness" stage="brightness" score={scores[0]}></ScoreCard>
+            <ScoreCard key="contrast" stage="contrast" score={scores[1]}></ScoreCard>
+            <ScoreCard key="saturation" stage="saturation" score={scores[2]}></ScoreCard>
+            <ScoreCard key="grayscale" stage="grayscale" score={scores[3]}></ScoreCard>
+            <ScoreCard key="sepia" stage="sepia" score={scores[4]}></ScoreCard>
+            <ScoreCard key="hue rotate" stage="hue rotate" score={scores[5]}></ScoreCard>
+            <ScoreCard key="blur" stage="blur" score={scores[6]}></ScoreCard>
             {/* <div className="scores-container">
                 {Object.entries(scores).map(([stage, score]) => (
                     <ScoreCard key={stage} stage={stage} score={score} />
