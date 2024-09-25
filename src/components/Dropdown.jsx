9f4900a5e3b27ed018aee5106d9588ed60bcfc13
Zhoucai <nizhoucai@gmail.com>
2024-09-24 23:16:56 -0700

    good enough for now

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import { IconDots } from '@tabler/icons-react';
-import { useDispatch, useSelector } from 'react-redux';
-import { reportPhoto, setReportPhotoVis } from '../actions/photoListAction';
-
+import React, { useState } from "react";
+import { IconDots } from "@tabler/icons-react";
+import { useDispatch, useSelector } from "react-redux";
+import { reportPhoto, setReportPhotoVis } from "../actions/photoListAction";
 
 const Dropdown = ({ photoId, closeModal }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const signedInUserId = useSelector(state => state.user.info.id);
+  const signedInUserId = useSelector((state) => state.user.info.id);
   const dispatch = useDispatch();
 
   const handleClick = () => {
@@ -18,18 +17,19 @@ const Dropdown = ({ photoId, closeModal }) => {
     closeModal();
     dispatch(setReportPhotoVis(true));
     setIsOpen(!isOpen);
-  }
+  };
 
   return (
-    <div style={{ position: 'relative' }}>
+    <div style={{ position: "relative" }}>
       <IconDots className="hoverPointer" onClick={handleClick} />
       {isOpen && (
         <ul id="reportUl">
-          <li id="reportLi" onClick={handleReportClick} >Report</li>
+          <li id="reportLi" onClick={handleReportClick}>
+            Report
+          </li>
         </ul>
-      )
-      }
-    </div >
+      )}
+    </div>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,149 @@
-import React from 'react'
-import "../styles/header.scss"
-import domainLogo from "../assets/domain_logo.svg"
-import { useNavigate } from 'react-router-dom';
-import TodaysDate from './TodaysDate';
-import { useLocation } from 'react-router-dom';
-import ProfileIcon from './ProfileIcon';
-
+import React, { useState, useEffect } from "react";
+import "../styles/header.scss";
+import domainLogo from "../assets/domain_logo.svg";
+import { useNavigate } from "react-router-dom";
+import TodaysDate from "./TodaysDate";
+import { useLocation } from "react-router-dom";
+import ProfileIcon from "./ProfileIcon";
+import { IconMenu2, IconX } from "@tabler/icons-react";
 
 const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const [headerModalVisability, setHeaderModalVisability] = useState(false);
   const handleNavigate = (path) => {
     navigate(path);
   };
-  return (
 
-    <div className='headerContainer'>
-      <div className='contentContainer'>
-        <div className="logoDateContainer">
-          <img onClick={() => handleNavigate("/community")} src={domainLogo} alt="" width="150"></img>
-          {location.pathname.startsWith('/daily') && <TodaysDate />}
+  const [screenSize, setScreenSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
-        </div>
-        <nav className='headerNav'>
-          <button className="dailyButton blackButton" onClick={() => handleNavigate("/daily")}> Daily</button>
-          <button className="communityButton blackButton" onClick={() => handleNavigate("/community")}>Community</button>
-          <button className="dailyButton blackButton" onClick={() => handleNavigate("/leaderboard")}> Leaderboard</button>
-          <button className="tutorialButton whiteButton" onClick={() => handleNavigate("/tutorial")}>Tutorial</button>
-          {localStorage.getItem('token') === null ? <button className="loginButton whiteButton" onClick={() => handleNavigate("/login")}> Login </button> : <ProfileIcon />}
+  const headerModal = () => {
+    return (
+      <>
+        <nav className="headerNavModal">
+          <button onClick={() => handleNavigate("/daily")}>Daily</button>
+          <button onClick={() => handleNavigate("/community")}>
+            Community
+          </button>
+          <button onClick={() => handleNavigate("/leaderboard")}>
+            Leaderboard
+          </button>
+          <button onClick={() => handleNavigate("/tutorial")}>Tutorial</button>
+
+          {localStorage.getItem("token") === null ? (
+            <button
+              className="loginButton whiteButton"
+              onClick={() => handleNavigate("/login")}
+            >
+              Login
+            </button>
+          ) : (
+            <button
+              className="loginButton whiteButton"
+              onClick={() => handleNavigate("/login")}
+            >
+              Profile
+            </button>
+          )}
         </nav>
-      </div>
-    </div >
-  )
-}
+      </>
+    );
+  };
 
-export default Header
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div className="headerContainer">
+      <div className="contentContainer">
+        <div className="logoDateContainer">
+          <img
+            onClick={() => handleNavigate("/community")}
+            src={domainLogo}
+            alt=""
+            width="150"
+          ></img>
+          {location.pathname.startsWith("/daily") && <TodaysDate />}
+        </div>
+
+        {screenSize.width >= 910 ? (
+          <nav className="headerNav">
+            <button
+              className="dailyButton blackButton"
+              onClick={() => handleNavigate("/daily")}
+            >
+              Daily
+            </button>
+            <button
+              className="communityButton blackButton"
+              onClick={() => handleNavigate("/community")}
+            >
+              Community
+            </button>
+            <button
+              className="dailyButton blackButton"
+              onClick={() => handleNavigate("/leaderboard")}
+            >
+              Leaderboard
+            </button>
+            <button
+              className="tutorialButton whiteButton"
+              onClick={() => handleNavigate("/tutorial")}
+            >
+              Tutorial
+            </button>
+
+            {localStorage.getItem("token") === null ? (
+              <button
+                className="loginButton whiteButton"
+                onClick={() => handleNavigate("/login")}
+              >
+                Login
+              </button>
+            ) : (
+              <ProfileIcon />
+            )}
+          </nav>
+        ) : (
+          <>
+            <div className="menuButton">
+              {headerModalVisability ? (
+                <IconX
+                  className="menuButton"
+                  onClick={() =>
+                    setHeaderModalVisability(!headerModalVisability)
+                  }
+                />
+              ) : (
+                <IconMenu2
+                  className="menuButton"
+                  onClick={() =>
+                    setHeaderModalVisability(!headerModalVisability)
+                  }
+                />
+              )}
+            </div>
+            {headerModalVisability && headerModal()}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
